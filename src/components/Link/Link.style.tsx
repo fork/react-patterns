0cf4c322d0df47845e163e4b5d1954acc7038d4b
jeslage <johannes.eslage@fork.de>
2021-07-02 16:50:32 +0200

    Updated link styling

--- src/components/Link/Link.style.tsx
+++ src/components/Link/Link.style.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import { space } from '../../stylesheets';
 
 const StyledLink = styled.a`
   display: inline-flex;
   align-items: center;
+
+  svg {
+    margin-right: ${space('xxs')};
+  }
 `;
 
 export default StyledLink;
